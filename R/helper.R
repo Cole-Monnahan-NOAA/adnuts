dbcd1d3ae6616aea5ab1802e476b89d7e1cf489a
colemonnahan <monnahc@uw.edu>
2017-04-02 16:26:00 -0700

    Rework shinystan helper functions

--- R/helper.R
+++ R/helper.R
@@ -49,44 +49,43 @@ update_control <- function(control){
   message(paste0(x, sprintf("%.1f", time.total), ' seconds (Total)'))
 }
 
-#' Convert TMB output from \link{\code{run_mcmc}} into a \code{shinystan}
-#' object.
+#' Convert adnuts fit (named list) into a \code{shinystan} object.
 #'
 #' @details The shinystan packages provides several conversion functions
 #'   for objects of different types, such as stanfit classes (Stan ouput)
 #'   and simple arrays. For the latter, option NUTS information, such as
 #'   \code{sampler_params} can be passed. This function essentially extends
-#'   the functionality of \code{as.shinystan} to work specifically with TMB
-#'   MCMC lists. The user can thus explore their TMB model with
-#'   \code{launch_shinystan(as.shinystan.tmb(tmb.fit))} in the same way
+#'   the functionality of \code{as.shinystan} to work specifically with
+#'   fits from adnuts (TMB or ADMB). The user can thus explore their model
+#'   with \code{launch_shinystan(as.shinystan.tmb(fit))} in the same way
 #'   that Stan models are examined.
-#' @param tmb.fit Output list from \link{\code{run_mcmc}} for any of the
-#' three algorithms.
-#' @seealso launch_shinystan_tmb
+#' @param fit Output list from \link{\code{sample_tmb}} or
+#'   \link{\code{sample_admb}}.
+#' @seealso launch_shinytmb, launch_shinyadmb
 #' @return An S4 object of class shinystan. Depending on the algorithm
 #'   used, this list will have slight differences.
 #' @export
-as.shinystan.tmb <- function(tmb.fit){
-  if(tmb.fit$algorithm=="NUTS"){
-    sso <- with(tmb.fit, as.shinystan(samples, warmup=warmup, max_treedepth=max_treedepth,
+as.shinyadnuts <- function(fit){
+  if(fit$algorithm=="NUTS"){
+    sso <- with(fit, as.shinystan(samples, warmup=warmup, max_treedepth=max_treedepth,
              sampler_params=sampler_params, algorithm='NUTS', model_name=model))
-  } else if(tmb.fit$algorithm=="HMC"){
-    sso <- with(tmb.fit, as.shinystan(samples, warmup=warmup,
+  } else if(fit$algorithm=="HMC"){
+    sso <- with(fit, as.shinystan(samples, warmup=warmup,
              sampler_params=sampler_params, algorithm='HMC', model_name=model))
   } else {
-    sso <- with(tmb.fit, as.shinystan(samples, warmup=warmup,
+    sso <- with(fit, as.shinystan(samples, warmup=warmup,
              algorithm='RWM', model_name=model))
   }
   return(sso)
 }
 
-#' A high level wrapper to launch shinystan for a TMB MCMC list object.
+#' A high level wrapper to launch shinystan for a TMB fit.
 #'
 #' @details This function simply calls
 #'   \code{launch_shinystan(as.shinystan.tmb(tmb.fit))}.
 #' @export
-launch_shinystan_tmb <- function(tmb.fit){
-  launch_shinystan(as.shinystan.tmb(tmb.fit))
+launch_shinytmb <- function(fit){
+  launch_shinystan(as.shinyadnuts(fit))
 }
 
 #' Extract posterior samples from a TMB MCMC fit list.
@@ -107,37 +106,13 @@ extract_samples <- function(fit.tmb, inc_warmup=FALSE){
   return(invisible(as.data.frame(y)))
 }
 
+#' A high level wrapper to launch shinystan for a ADMB fit.
+#'
+#' @details This function simply calls
+#'   \code{launch_shinystan(as.shinystan.tmb(tmb.fit))}.
 #' @export
-as.shinystan.admb <- function(admb.fit){
-  sso <- with(admb.fit,
-    as.shinystan(samples, warmup=warmup, max_treedepth=max_treedepth,
-                 sampler_params=sampler_params, algorithm='NUTS',
-                 model_name=model))
-  ## pars2 <- array(0, dim=c(nrow(pars), 1, ncol(pars)))
-  ## pars2[,1,] <- as.matrix(pars)
-  ## dimnames(pars2) <-
-  ##   list(iter=1:nrow(pars), chains="chain:1",
-  ##        parameters=dimnames(pars)[[2]])
-  ## ss <- monitor(sims=pars2)
-  ## y <- vector("list", length=length(dimnames(pars2)[[3]]))
-  ## names(y) <- dimnames(pars2)[[3]]
-  ## z <- lapply(y, function(x) x=numeric(0))
-  ## sso <-
-  ##   shinystan:::shinystan(
-  ##   model_name=model.name, param_names=names(pars), param_dims=z,
-  ##   posterior_sample=pars2, sampler_params=list(adapt),
-  ##   summary=ss, n_chain=1, n_iter=nrow(pars),
-  ##   n_warmup=nrow(pars)/2, model_code='NA',
-  ##   misc=list(max_td=12, stan_method='sampling',
-  ##             stan_algorithm='NUTS',
-  ##             sso_version=utils::packageVersion('shinystan')))
-
-    return(sso)
-}
-
-#' @export
-launch_shinystan_admb <- function(admb.fit){
-  launch_shinystan(as.shinystan.admb(admb.fit))
+launch_shinyadmb <- function(fit){
+  launch_shinystan(as.shinyadnuts(fit))
 }
 
 #' Write matrix of samples to a binary .psv file.
