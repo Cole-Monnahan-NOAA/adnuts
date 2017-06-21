645ad619b9134ee9463b4cb932dc25f2ea683dd8
colemonnahan <monnahc@uw.edu>
2017-06-21 13:17:03 -0700

    Temp comment out mass adaptation code

--- R/adaptation.R
+++ R/adaptation.R
@@ -53,48 +53,48 @@ rotate_space <- function(fn, gr, M, theta.cur){
 
 
 
-## Quick sketch of algorithm used to test.
-w1 <- 75
-w2 <- 50
-w3 <- 25
-aws <- w2
-anw <- w1+w2
-warmup <- 400
-## Initialize algorithm
-M <- c(.5,.5)
-sd <- .1
-m <- m0 <- rnorm(2, sd=sd)
-s <- s0 <- c(0,0)
-i <- 1
-k <- 2
-X <- matrix(NA, warmup, 2)
-for(i in 1:warmup){
-  X[i,] <- theta <- rnorm(2, sd=sd)
-  ## If in slow phase, update running estimate of variances
-  if(slow_phase(i,warmup, w1, w3)){
-    ## The Welford running variance calculation, see
-    ## https://www.johndcook.com/blog/standard_deviation/
-    ## Save last iteration
-    m0 <- m; s0 <- s
-    ## Update M and S
-    m <- m0+(theta-m0)/k
-    s <- s0+(theta-m0)*(theta-m)
-    k <- k+1
-  }
-  ## If at end of adaptation window, update the mass matrix to the estimated
-  ## variances
-  if(i==anw & slow_phase(i,warmup, w1, w3)){
-    ## Update the mass matrix
-    M <- s/(k-1)
-    message(paste("Updating M:", i, M[1]))
-    ## temp <- rotate_space(fn, gr, M, theta.cur)
-    ## Reset the running variance calculation
-    k <- 2; m0 <- m <- theta; s0 <- s <- 0
-    ## Calculate the next end window. If this overlaps into the final fast
-    ## period, it will be stretched to that point (warmup-w3)
-    aws <- 2*aws
-    anw <- compute_next_window(i, anw, warmup, w1, aws, w3)
-  }
-}
-M
-apply(X[125:375,],2, var)
+## ## Quick sketch of algorithm used to test.
+## w1 <- 75
+## w2 <- 50
+## w3 <- 25
+## aws <- w2
+## anw <- w1+w2
+## warmup <- 400
+## ## Initialize algorithm
+## M <- c(.5,.5)
+## sd <- .1
+## m <- m0 <- rnorm(2, sd=sd)
+## s <- s0 <- c(0,0)
+## i <- 1
+## k <- 2
+## X <- matrix(NA, warmup, 2)
+## for(i in 1:warmup){
+##   X[i,] <- theta <- rnorm(2, sd=sd)
+##   ## If in slow phase, update running estimate of variances
+##   if(slow_phase(i,warmup, w1, w3)){
+##     ## The Welford running variance calculation, see
+##     ## https://www.johndcook.com/blog/standard_deviation/
+##     ## Save last iteration
+##     m0 <- m; s0 <- s
+##     ## Update M and S
+##     m <- m0+(theta-m0)/k
+##     s <- s0+(theta-m0)*(theta-m)
+##     k <- k+1
+##   }
+##   ## If at end of adaptation window, update the mass matrix to the estimated
+##   ## variances
+##   if(i==anw & slow_phase(i,warmup, w1, w3)){
+##     ## Update the mass matrix
+##     M <- s/(k-1)
+##     message(paste("Updating M:", i, M[1]))
+##     ## temp <- rotate_space(fn, gr, M, theta.cur)
+##     ## Reset the running variance calculation
+##     k <- 2; m0 <- m <- theta; s0 <- s <- 0
+##     ## Calculate the next end window. If this overlaps into the final fast
+##     ## period, it will be stretched to that point (warmup-w3)
+##     aws <- 2*aws
+##     anw <- compute_next_window(i, anw, warmup, w1, aws, w3)
+##   }
+## }
+## M
+## apply(X[125:375,],2, var)
