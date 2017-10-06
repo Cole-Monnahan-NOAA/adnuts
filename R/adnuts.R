eb4c6b45ccf41e477478ff12e93899727761d38d
colemonnahan <monnahc@uw.edu>
2017-10-06 17:37:58 -0300

    Add file for package description and imports

---
+++ R/adnuts.R
@@ -0,0 +1,11 @@
+#' adnuts: No-U-turn sampling for Template Model Builder and AD Model Builder
+#'
+#' The \pkg{ss3sim} \R package is designed to facilitate rapid, reproducible,
+#' and flexible simulation with the widely-used Stock Synthesis 3 (SS3)
+#' statistical catch-at-age stock assessment framework.
+#'
+#' @docType package
+#' @name adnuts
+#' @importFrom stats rnorm runif cov
+#' @importFrom utils read.csv read.table write.table
+NULL
