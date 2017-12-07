0d9a8769d6df0e2e1df644e1c1d7f4d0f2ec0b3a
colemonnahan <monnahc@uw.edu>
2017-12-07 13:12:09 -0300

    Add bibliography to vignette

---
+++ vignettes/auto/refs.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "refs"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "betancourt2014"
+    "kristensen2016"
+    "fournier2012"
+    "monnahan2017"
+    "carpenter2017"
+    "hoffman2014"
+    "neal2011"
+    "betancourt2016"
+    "betancourt2017intro"
+    "stan2017")))
+
