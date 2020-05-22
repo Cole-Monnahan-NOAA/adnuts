d7bb40ec2f1a40ae0cff04ef273b79b9e42431ae
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2020-05-22 11:38:38 -0700

    Overhaul and update vignette

--- vignettes/auto/refs.el
+++ vignettes/auto/refs.el
@@ -2,6 +2,8 @@
  "refs"
  (lambda ()
    (LaTeX-add-bibitems
+    "monnahan2018"
+    "monnahan2019"
     "betancourt2014"
     "kristensen2016"
     "fournier2012"
@@ -11,5 +13,6 @@
     "neal2011"
     "betancourt2016"
     "betancourt2017intro"
-    "stan2017")))
+    "stan2017"))
+ :bibtex)
 
