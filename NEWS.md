811ac836b25253fcd437f1e19bb90c84dac751cb
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2020-08-07 09:22:54 -0700

    Update doc and vignette and demo

--- NEWS.md
+++ NEWS.md
@@ -2,45 +2,71 @@
 adnuts 1.1.0 (2020-07-13)
 ------------------------------------------------------------------------
 
+* Change from `sample_admb` to `sample_nuts` and `sample_rwm` to
+  run the NUTS and RWM algoriths, respectively.
+  
 * Rework metric options to allow user to access ADMB 12.2's new
   dense mass matrix adaptation scheme. Added new section
   demonstrating this in the vignette.
-* Add more control to 'sample_admb' via 'skip_monitor',
+  
+* Add more control to via 'skip_monitor',
   'skip_unbounded', and 'skip_optimization' arguments
+  
 * Remove TMB references from documentation and vignette, 
   instead pointing users to package 'tmbstan', and collate
   deprecated R code into a single file
+  
 * Migrate to new github repo: github.com/Cole-Monnahan-NOAA per
   NOAA's policy
+  
 * Add testing via testthat package
+
 * Turn on calculation of ESS and Rhat manually, which get used in
   subsequent functions
+  
 * Created S3 class 'adfit' and generic methods print, summary,
   and plot
-* Updated pairs_admb to have an 'order' argument for quickly
+  
+* Updated `pairs_admb` to have an 'order' argument for quickly
   plotting slow/fast parameters
-* Add new function plot_marginals for quickly plotting posterior
+  
+* Add new function `plot_marginals` for quickly plotting posterior
   histograms
-* Add new function plot_sampler_params to plot NUTS sampling  
+  
+* Add new function `plot_sampler_params` to plot NUTS sampling
+
 * Make parallel the default and deprecate the 'parallel'
   argument.
+  
 * Fix bug in parallel path which failed when it was absolute. Now
   can be relative or absolute. 
+  
 * Add check for valid version of ADMB
+
 * Minor bug fixes and documentation updates
+
 * Improve error handling and testing routines
 
 ------------------------------------------------------------------------
 adnuts 1.0.1 (2019-03-15) 
 ------------------------------------------------------------------------
 
-* Update ADMB algorithms to use "-maxfn 0 -phase 1000" instead of "-noest". This helps with Stock Synthesis and likely other models where some initialization is skipped with -noest which can lead to unusual and undesirable behavior. Also changed behavior with inits=NULL to pull MLE values from the admodel.hes file instead of pulling from the .par file for inits. This fixes some models when negative phases are used.
+* Update ADMB algorithms to use "-maxfn 0 -phase 1000" instead of
+  "-noest". This helps with Stock Synthesis and likely other
+  models where some initialization is skipped with -noest which
+  can lead to unusual and undesirable behavior. Also changed
+  behavior with inits=NULL to pull MLE values from the
+  admodel.hes file instead of pulling from the .par file for
+  inits. This fixes some models when negative phases are used.
 
-* Add function check_identifiable which examines a .hes file and reports which parameters are not well identified.
+* Add function check_identifiable which examines a .hes file and
+  reports which parameters are not well identified.
 
-* Add function sample_inits to generate inits from a previous fitted object.
+* Add function sample_inits to generate inits from a previous
+  fitted object.
 
-* Read in MLE values from the .hes file when inits=NULL, instead of from the .par file. 
+* Read in MLE values from the .hes file when inits=NULL, instead
+  of from the .par file.
 
 * Add informative errors for common issues.
 
--- inst/demo.R
+++ inst/demo.R
@@ -22,7 +22,7 @@ setwd('..')
 init <-  lapply(1:3, function(i) rnorm(2))
 init <- NULL # uses MLEs -- not recommended!
 init <- function() rnorm(2)
-fit <- sample_admb(model='simple', init=init, path=path)
+fit <- sample_nuts(model='simple', init=init, path=path, cores=1)
 fit$cmd[1] # this is the command line arguments used
 ## Merged chains after discarding warmup phase
 post <- extract_samples(fit)
@@ -33,7 +33,7 @@ post <- extract_samples(fit)
 str(post)
 ## A list with MLE stuff, after rerunning with Hessian
 setwd(path); system('simple'); setwd('..')
-fit <- sample_admb(model='simple', init=init, path=path)
+fit <- sample_nuts(model='simple', init=init, path=path, cores=1)
 str(fit$mle)
 pairs_admb(fit) # modified pairs just for ADMB fits like this
 ## Can also use ShinyStan (make sure to exit it)
@@ -45,10 +45,7 @@ pairs_admb(fit) # modified pairs just for ADMB fits like this
 ## call runs on all chains and can be used directly for inference. You can
 ## set mceval=TRUE here, or run your model later manually as typically
 ## done.
-library(snowfall)
-cores <- parallel::detectCores()-1
-fit <- sample_admb(model='simple', init=init, path=path,
-                   parallel=TRUE, cores=cores, mceval=TRUE)
+fit <- sample_nuts(model='simple', init=init, path=path, mceval=TRUE)
 
 ## The default is use an adaptive mass matrix. If we want to use the MLE
 ## covariance as the mass matrix, set it using control. This *should* make
@@ -56,17 +53,18 @@ fit <- sample_admb(model='simple', init=init, path=path,
 ## optimize the model with flag -hbf 1
 setwd(path); system('simple -hbf 1'); setwd('..')
 fit <- sample_admb(model='simple', init=init, path=path,
-                    parallel=TRUE, cores=cores, control=list(metric='mle'))
+                    control=list(metric='mle'))
 
 
 ## Can also use a slightly modified version of the original Metropolis MCMC
 ## algorithm (Random Walk Metropolis or RWM). Here we really want to use
 ## the MLE covariance. Since we ran above with hbf=1 we need to rerun model
-## to recreate the .hes and .cov files.
-setwd(path); system('simple'); setwd('..')
-fit <- sample_admb(model='simple', init=init, path=path, algorithm='RWM',
-                    iter=200000, thin=100, mceval=TRUE,
-                    parallel=T, cores=cores, control=list(metric='mle'))
+## to recreate the .hes and .cov files. Can do this with
+## skip_optimization argument
+fit <- sample_rwm(model='simple', init=init, path=path,
+                  skip_optimization=FALSE,
+                  iter=200000, thin=100, mceval=TRUE)
+
 
 ## Can also specify a duration argument for capping the run time. This is
 ## useful e.g. if you want to run overnight but have results by 8am.  Here
@@ -74,9 +72,10 @@ fit <- sample_admb(model='simple', init=init, path=path, algorithm='RWM',
 ## enough to do the warmup or it'll throw an error. Normally we wouldn't
 ## use thinning for NUTS but only to demonstrate here since the model runs
 ## so fast.
-fit <- sample_admb(model='simple', init=init, path=path, algorithm='NUTS',
-                    warmup=200, iter=20000000, thin=100, duration=.5,
-                    parallel=TRUE, cores=cores)
+fit <- sample_nuts(model='simple', init=init, path=path,
+                   warmup=200, iter=20000000, thin=100,
+                   duration=.5)
+
 ## Notice that the variable chain lengths are truncated to the minimum, and
 ## then merged together.
 str(extract_samples(fit))
@@ -84,54 +83,8 @@ str(extract_samples(fit, as.list=TRUE))
 
 ## Remove folder
 unlink(path, TRUE)
-### End of ADMB example
 
 
+### End of demo
 
-### --------------------------------------------------
-### TMB example: Note, this is large replaced by tmbstan. See that package
-### for more info on using Stan with TMB models.
 
-## Note that this model does not use any box constraints. These can be
-## passed to sample_tmb just like with nlminb. Also, there are no explicit
-## priors in this model, which can be influential particularly for the
-## logsd params. The user is solely responsible for properly defining a
-## Bayesian model, including priors and bounds.
-library(TMB)
-TMB::runExample("simple")
-
-## init can be a function, or a list of lists, or NULL to use starting
-## values in obj.
-init <- function() list(mu=u, beta=beta, logsdu=0, logsd0=0)
-seeds <- 1:3
-## The default is to run 3 chains, 50% warmup, and use diagonal mass matrix
-## adapation. This is the same as rtan.
-fit <- sample_tmb(obj=obj, init=init, seeds=seeds)
-
-## Can also run in parallel, but the DLL needs to be available in this
-## folder since each node calls MakeADFun again to remake obj.  This
-## usually is your working directory.
-path <- system.file("examples", package = "TMB")
-## This line is only needed b/c of how TMB::runexample works above.
-compile(file.path(path,'simple.cpp'))
-library(snowfall)
-fit <- sample_tmb(obj=obj, seeds=seeds, init=init,
-                   parallel=TRUE, cores=cores, path=path)
-
-## Extract samples like this
-post <- extract_samples(fit)
-## You can use these however you want. For instance, loop through each
-## saved row and call object$report(post1[i,]) to do any calculations or
-## projections (equivalent to -mceval in ADMB).
-
-## Use rstan functions to calculate effective sample sizes (ESS) and Rhat
-## (potential scale reduction.
-mon <- rstan::monitor(fit$samples, print=FALSE)
-Rhat <- mon[,"Rhat"]
-max(Rhat)
-ess <- mon[, 'n_eff']
-min(ess)
-
-## Or explore with shinystan. Make sure to click "Save and close" to exit
-## properly.
-launch_shinytmb(fit)
