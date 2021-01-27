ca72a72ef7901b6790910666ed55c10523eb60c7
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-01-27 15:50:35 -0800

    Try to add executables to get CI to run

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,9 @@
 library(testthat)
 library(adnuts)
 
+if(!.Platform$OS.type=='windows'){
+  ## Swap out the Win executable for unix one
+  file.rename('simple/simple.exe', 'simple/simple_windows.exe')
+  file.copy('simple/simple_unix', to='simple/simple')
+}
 test_check("adnuts")
