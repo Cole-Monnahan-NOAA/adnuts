7c1efa94ec38b00231bd8c932920baf61bb38cfa
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-01-28 08:36:01 -0800

    Try installing admb and compile test model 4

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -4,19 +4,23 @@
 oldwd <- getwd()
 message(oldwd)
 setwd('../simple')
-if(!.Platform$OS.type=='windows'){
-  ## Swap out the Win executable for unix one
-  if(!file.exists('../simple/simple_unix'))
-    stop("Failed to copy Unix executable b/c not found")
-  file.copy('../simple/simple_unix', to='../simple/simple')
-  system('sudo chmod a+x simple') # give permission
-  system('./simple -nox')
-} else {
-  if(!file.exists('../simple/simple_windows.exe'))
-    stop("Failed to copy Windows executable b/c not found")
-  file.copy('../simple/simple_windows.exe',
-            '../simple/simple.exe')
-  system("simple -nox")
-}
+system('where admb')
+system('admb simple')
+system('sudo chmod a+x simple') # give permission
+system('./simple -nox')
+## if(!.Platform$OS.type=='windows'){
+##   ## Swap out the Win executable for unix one
+##   if(!file.exists('../simple/simple_unix'))
+##     stop("Failed to copy Unix executable b/c not found")
+##   file.copy('../simple/simple_unix', to='../simple/simple')
+##   system('sudo chmod a+x simple') # give permission
+##   system('./simple -nox')
+## } else {
+##   if(!file.exists('../simple/simple_windows.exe'))
+##     stop("Failed to copy Windows executable b/c not found")
+##   file.copy('../simple/simple_windows.exe',
+##            '../simple/simple.exe')
+##   system("simple -nox")
+## }
 print(list.files())
 setwd(oldwd)
