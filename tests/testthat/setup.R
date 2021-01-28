ce3caa1abeaeb56c5e84c4386f44ce387bfe4cff
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-01-27 16:57:40 -0800

    Fix so it optimizes model once in setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,13 +1,19 @@
 
 ## For now I'm providing copies of executables and just swapping
 ## out. Only temporary and really bad form.
+oldwd <- getwd()
+setwd('../simple')
 if(!.Platform$OS.type=='windows'){
   ## Swap out the Win executable for unix one
   if(!file.exists('../simple/simple_unix'))
     stop("Failed to copy Unix executable b/c not found")
   file.copy('../simple/simple_unix', to='../simple/simple')
+  system('./simple -nox')
 } else {
   if(!file.exists('../simple/simple_windows.exe'))
     stop("Failed to copy Windows executable b/c not found")
-  file.copy('../simple/simple_windows.exe', '../simple/simple.exe')
+  file.copy('../simple/simple_windows.exe',
+            '../simple/simple.exe')
+  system("simple -nox")
 }
+setwd(oldwd)
