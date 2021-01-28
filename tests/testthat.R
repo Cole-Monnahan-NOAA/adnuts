dda146b89234b0759512b672178a4604f60192a5
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-01-27 16:25:38 -0800

    More tweaks to get execs going to pass CI

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,9 @@ library(adnuts)
 
 if(!.Platform$OS.type=='windows'){
   ## Swap out the Win executable for unix one
-  file.rename('simple/simple.exe', 'simple/simple_windows.exe')
-  file.copy('simple/simple_unix', to='simple/simple')
+  if(!file.exists('../simple/simple_unix'))
+    stop("Failed to copy Unix executable b/c not found")
+  file.rename('../simple/simple.exe', '../simple/simple_windows.exe')
+  file.copy('../simple/simple_unix', to='../simple/simple')
 }
 test_check("adnuts")
